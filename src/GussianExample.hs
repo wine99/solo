ab188ae56b40aa89992cdc9fd3df9591526d1739
WenyuanXu <wenyuan.xu@cs.au.dk>
2023-06-01 13:21:37 +0200

    WIP: examples

---
+++ src/GussianExample.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE
+    AllowAmbiguousTypes
+   ,DataKinds
+   ,FlexibleContexts
+   ,GADTs
+   ,GeneralizedNewtypeDeriving
+   ,KindSignatures
+   ,PartialTypeSignatures
+   ,PolyKinds
+   ,RankNTypes
+   ,ScopedTypeVariables
+   ,TypeApplications
+   ,TypeFamilies
+   ,TypeOperators
+   ,TypeSynonymInstances
+   ,TypeFamilyDependencies
+   ,UndecidableInstances
+   ,RebindableSyntax
+   ,EmptyCase
+   #-}
+
+module GussianExample where
+
+import Prelude hiding (sum) -- 默认导入的模块
+-- import qualified Prelude as P   -- 别名为 P
+import Data.TypeLits as TL
+import Data.Proxy
+
+import System.Random
+import qualified System.Random.MWC as MWC
+import qualified Statistics.Distribution.Laplace as Lap
+import Statistics.Distribution (ContGen(genContVar))
+import System.Random.MWC (createSystemRandom)
+
+import SensitivitySafe
+import PrivacySafe
+import Primitives
+import StdLib
+import Text.Read (readMaybe)
+
+import qualified Data.Map.Strict as Map
+import qualified GHC.List as List
+import Sensitivity (SList(SList_UNSAFE), SDouble (D_UNSAFE))
+
+import System.IO
+
+readDoublesFromFile :: FilePath -> IO (Maybe [Double])
+readDoublesFromFile filePath = do
+    content <- readFile filePath
+    let linesOfContent = lines content
+    return $ mapM readMaybe linesOfContent
+
